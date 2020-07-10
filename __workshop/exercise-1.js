18ab258a27802769a19881ffbcc88ff137b7d812
Jordan Gabay <gabay.jordan@outlook.com>
2020-07-10 11:47:27 -0400

    Exercise 1 Complete

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -5,10 +5,22 @@ const complicatedArray = ['cucumber', 44, true];
 
 const makeAllCaps = (array) => {
   // write some code
+  myPromise = new Promise((resolve, reject) => {
+    if(array.every(word => typeof word === 'string')) {
+    resolve(array.map(word => word.toUpperCase()))
+    } else {
+    reject(`Error: ${array} contains at least one element that is not a string!`)
+    }
+  })
+  return myPromise
 };
 
 const sortWords = (array) => {
   // write some code
+  myPromise = new Promise((resolve) => {
+    resolve(array.sort())
+  })
+  return myPromise
 };
 
 // Calling (testing)
