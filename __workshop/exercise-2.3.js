cc98c773c436f32ac14eed7db92dcdfb777c941e
Jordan Gabay <gabay.jordan@outlook.com>
2020-07-10 16:46:32 -0400

    2.3 Complete

--- __workshop/exercise-2.3.js
+++ __workshop/exercise-2.3.js
@@ -5,3 +5,22 @@
 const request = require('request-promise');
 
 // get the code you wrote in 2.2 and paste it here...
+
+const greeting = (langCode) => {
+    return request(`https://journeyedu.herokuapp.com/hello/${langCode}`) // 1
+      .then((response) => {
+        return JSON.parse(response)
+      })
+      
+      .catch((err) => {
+        return err.error ? JSON.parse(err.error) : err;
+      });
+  };
+  
+  // Testing
+  greeting('fr').then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
+  
+  // 3
+  greeting('es').then((result) => console.log(result));
+  greeting('it').then((result) => console.log(result));
+  greeting('sp').then((result) => console.log(result));
