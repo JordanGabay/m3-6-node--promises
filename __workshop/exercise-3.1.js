ecf40264933cd86b7703a4daac873b59dde3f591
Jordan Gabay <gabay.jordan@outlook.com>
2020-07-12 19:29:51 -0400

    3.1 Complete

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,24 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require('request-promise');
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
+  return request('http://api.open-notify.org/iss-now.json')
+  .then(response => JSON.parse(response))
+  .then(parsedResponse => {
+    const currentPosition = parsedResponse.iss_position
+    parsedResponse.iss_position = {
+      lat: currentPosition.latitude,
+      lon: currentPosition.longitude
+    }
+    return parsedResponse
+  })
+  .catch((err) => {
+    return err.error ? JSON.parse(err.error) : err;
+  });
   // write some code...
 };
+
+getIssPosition().then((result) => console.log(result));
